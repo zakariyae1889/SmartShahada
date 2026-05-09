40db20796a06099371a3d8dd46fc4f14778450a0
Abbihzakariyae <abbih1889@gmail.com>
2026-05-09 22:54:21 +0000

    commit-update

--- server/database/migrations/2026_05_09_223437_create_regions_table.php
+++ server/database/migrations/2026_05_09_223437_create_regions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('regions', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
             $table->timestamps();
         });
     }
